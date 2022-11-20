f1bfc978328b38f8ea9bfc99e5a25df72f3e74ba
Jess <jessjelardo@gmail.com>
2022-11-20 17:29:02 +0800

    fix cors issue

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+var cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -30,11 +33,37 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+    // bcrypt.compare('apple', '$2b$10$5Eo6l2is/gHco8hDhXXdKe3WIDR9vFOf39S.o9TZZi69QDgfLV0xa', function(err, result) {
+    //     // result == true
+    //     console.log(result)
+    // });
+
     if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-        res.json('success')
+        res.json(database.users[0])
     } else {
         res.status(400).json('error logging in')
     }
+})
+
+app.post('/signup', (req, res) => {
+    const { name, email, password } = req.body;
+    // unary operator
+    id = (database.users[database.users.length - 1].id)
+    id_incrementor =  (+id) + 1;
+
+    // bcrypt.hash(password, saltRounds=10, (err, hash) => {
+    //     console.log(hash)
+    // });
+
+    database.users.push({
+            id: id_incrementor.toString(),
+            name: name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+    })
+    res.json(database.users[database.users.length - 1])
 })
 
 app.get('/profile/:id', (req, res) => {
@@ -49,22 +78,6 @@ app.get('/profile/:id', (req, res) => {
     if(!found){
         res.status(404).json("no such user")
     }
-})
-
-app.post('/signup', (req, res) => {
-    const { name, email, password } = req.body;
-    // unary operator
-    id = (database.users[database.users.length - 1].id)
-    id_incrementor =  (+id) + 1;
-    database.users.push({
-            id: id_incrementor.toString(),
-            name: name,
-            email: email,
-            password: password,
-            entries: 0,
-            joined: new Date()
-    })
-    res.json(database.users)
 })
 
 app.post('/image', (req, res) => {
