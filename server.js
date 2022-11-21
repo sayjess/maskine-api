dee28e6748e080311f1dcfab7a6506915a835025
Jess <jessjelardo@gmail.com>
2022-11-22 02:45:01 +0800

    adding db connection

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-graceful-05872',
-      port : 5432,
-      user : 'postgres',
-      password : 'test',
-      database : 'maskine'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
